9710aa42992dda9b4fe4d379882d2d9fade0806a
Garrett Tucker <garrettatucker@gmail.com>
2019-08-27 17:27:02 -0500

    finished K Nearest Neighbor Practice

--- KNN.py
+++ KNN.py
@@ -6,7 +6,6 @@ import numpy as np
 from sklearn import linear_model, preprocessing
 
 data = pd.read_csv("car.data")
-print(data.head())
 
 '''TURNS LIST INFORMATION INTO INTEGERS TO PROCESS'''
 le = preprocessing.LabelEncoder()
@@ -27,4 +26,19 @@ y = list(classes)
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-print(x_train, y_test)
+model = KNeighborsClassifier(n_neighbors = 7)
+
+model.fit(x_train, y_train)
+
+acc = model.score(x_test, y_test)
+
+print(acc)
+
+
+predicted = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+    print("Predicted: ",  names [predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 7, True)
+    print("N: ", n)
