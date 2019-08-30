715548b4b03efa37c0f3db5622704c84c9287844
Garrett Tucker <garrettatucker@gmail.com>
2019-08-30 11:52:07 -0500

    started html template for user input

--- linear_regression.py
+++ linear_regression.py
@@ -1,3 +1,5 @@
+from flask import Flask
+from flask import render_template
 import tensorflow
 import keras
 import pandas as pd
@@ -10,52 +12,59 @@ import pickle
 from matplotlib import style
 
 
-data = pd.read_csv("student-mat.csv", sep=";")
-data = data[["G1","G2","G3","studytime","failures","absences"]]
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
-predict = "G3"
+@app.route("/")
+def index():
+    data = pd.read_csv("student-mat.csv", sep=";")
+    data = data[["G1","G2","G3","studytime","failures","absences"]]
 
-x = np.array(data.drop([predict], 1))
-y = np.array(data[predict])
+    predict = "G3"
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+    x = np.array(data.drop([predict], 1))
+    y = np.array(data[predict])
 
-'''
-
-TRAINING FOR LOOP TO GET HIGHEST ACCURACY
-best = 0
-for _ in range(30):
-#splits test data into chunks of ten percent of the original test data amount
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-    linear = linear_model.LinearRegression()
+    '''
 
-    linear.fit(x_train, y_train)
-    acc = linear.score(x_test,y_test)
-    print(acc)
+    TRAINING FOR LOOP TO GET HIGHEST ACCURACY
+    best = 0
+    for _ in range(30):
+    #splits test data into chunks of ten percent of the original test data amount
+        x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-    if acc > best:
-        best = acc
-        with open("studentmodel.pickle", "wb") as f:
-            pickle.dump(linear, f)
-'''
+        linear = linear_model.LinearRegression()
 
-pickle_in = open("studentmodel.pickle", "rb")
-linear = pickle.load(pickle_in)
+        linear.fit(x_train, y_train)
+        acc = linear.score(x_test,y_test)
+        print(acc)
 
-print("Coefficient: \n", linear.coef_)
-print("intercept: \n", linear.intercept_)
+        if acc > best:
+            best = acc
+            with open("studentmodel.pickle", "wb") as f:
+                pickle.dump(linear, f)
+    '''
 
-predictions = linear.predict(x_test)
+    pickle_in = open("studentmodel.pickle", "rb")
+    linear = pickle.load(pickle_in)
 
-for x in range(len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
+    print("Coefficient: \n", linear.coef_)
+    print("intercept: \n", linear.intercept_)
+
+    predictions = linear.predict(x_test)
+
+    for x in range(len(predictions)):
+        print(predictions[x], x_test[x], y_test[x])
 
 
-'''CREATES SCATERPLOT FOR DATA'''
-p = "G1"
-style.use("ggplot")
-pyplot.scatter(data[p], data["G3"])
-pyplot.xlabel(p)
-pyplot.ylabel("final grade")
-pyplot.show()
+    '''CREATES SCATERPLOT FOR DATA'''
+    p = "G1"
+    style.use("ggplot")
+    pyplot.scatter(data[p], data["G3"])
+    pyplot.xlabel(p)
+    pyplot.ylabel("final grade")
+    pyplot.show()
+
+    app.run()
